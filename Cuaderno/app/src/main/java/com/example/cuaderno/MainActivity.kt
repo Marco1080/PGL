bdaf7b4d6ea3e15589de721cd5385359109bb88d
Marco <93841465+Marco1080@users.noreply.github.com>
2024-11-14 01:17:25 +0000

    Dibujar cuaderno.

---
+++ Cuaderno/app/src/main/java/com/example/cuaderno/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.cuaderno
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val spinner = findViewById<Spinner>(R.id.spinnerColor)
+        val items = listOf("Rojo", "Verde", "Azul")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, items)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+    }
+}
