ca3c230d2d0b8a5dad1ffe70445f162b68d98771
Marco <93841465+Marco1080@users.noreply.github.com>
2024-12-08 20:23:46 +0000

    Perfil creado

--- InstaViajes/app/src/main/java/com/example/instaviajes/MenuActivity.kt
+++ InstaViajes/app/src/main/java/com/example/instaviajes/MenuActivity.kt
@@ -1,33 +1,20 @@
 package com.example.instaviajes
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LoginActivity : AppCompatActivity() {
+class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_menu)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        val goToRegister = findViewById<TextView>(R.id.goToRegister)
-        goToRegister.setOnClickListener{
-            val pantallaRegistro = Intent(this, RegisterActivity::class.java)
-            startActivity(pantallaRegistro)
-        }
-
-        val goToMenu = findViewById<TextView>(R.id.btnLogin)
-        goToMenu.setOnClickListener{
-            val pantallaMenu = Intent(this, ProfileActivity::class.java)
-            startActivity(pantallaMenu)
         }
     }
 }
