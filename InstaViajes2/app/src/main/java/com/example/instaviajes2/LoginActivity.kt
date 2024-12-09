4b7b9f15ede5a96ebcff56714facad8bef85156a
Marco <93841465+Marco1080@users.noreply.github.com>
2024-12-09 21:58:53 +0000

    Proyecto PGL desde cero.

---
+++ InstaViajes2/app/src/main/java/com/example/instaviajes2/LoginActivity.kt
@@ -0,0 +1,37 @@
+package com.example.instaviajes2
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val imageView: ImageView = findViewById(R.id.backgroundImage)
+
+        val images = arrayOf(
+            R.drawable.montania1,
+            R.drawable.montania2,
+            R.drawable.playa1,
+            R.drawable.playa2
+        )
+
+        val randomImage = images[Random.nextInt(images.size)]
+
+        imageView.setImageResource(randomImage)
+    }
+}
