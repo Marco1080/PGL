135f7324ed13d8504518826c22e4bbc2134a6558
Marco <93841465+Marco1080@users.noreply.github.com>
2024-12-14 18:30:18 +0000

    Menu creado

--- InstaViajes2/app/src/main/java/com/example/instaviajes2/LoginActivity.kt
+++ InstaViajes2/app/src/main/java/com/example/instaviajes2/LoginActivity.kt
@@ -2,12 +2,11 @@ package com.example.instaviajes2
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.enableEdgeToEdge
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import kotlin.random.Random
 
 class LoginActivity : AppCompatActivity() {
@@ -15,13 +14,6 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
 
         val imageView: ImageView = findViewById(R.id.backgroundImage)
 
@@ -31,15 +23,19 @@ class LoginActivity : AppCompatActivity() {
             R.drawable.playa1,
             R.drawable.playa2
         )
-
         val randomImage = images[Random.nextInt(images.size)]
-
         imageView.setImageResource(randomImage)
 
         val noRegistrado = findViewById<TextView>(R.id.noRegistrado)
-        noRegistrado.setOnClickListener{
+        noRegistrado.setOnClickListener {
             val pantallaRegistro = Intent(this, RegisterActivity::class.java)
             startActivity(pantallaRegistro)
+        }
+
+        val buttonMenu = findViewById<Button>(R.id.buttonMenu)
+        buttonMenu.setOnClickListener {
+            val pantallaMenu = Intent(this, MenuActivity::class.java)
+            startActivity(pantallaMenu)
         }
     }
 }
